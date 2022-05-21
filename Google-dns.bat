dbca54460eac025ff8b837e3bacd32afa1ad9df3
Sai Kumar Balabhadruni <31947788+SaiKumarBalabhadruni@users.noreply.github.com>
2022-05-21 22:09:43 +0530

    Update Google-dns.bat

--- Google-dns.bat
+++ Google-dns.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 
-REM Project Name: Move to Google DNS
-REM URL: https://github.com/SaiKumarBalabhadruni
-REM Author: Sai Kumar balabhadruni
-REM Website: skb.earth
-REM Description: Updates Primary DNS to Google DNS
+REM Project Name: Google's Public DNS updater
+REM Sai Kumar balabhadruni
+REM https://github.com/SaiKumarBalabhadruni
+REM wiselifer.com and skb.earth
+REM Description:  Change computer DNS to Google's Public DNS
 
 netsh interface ipv4 set dns name="Ethernet0" static 8.8.8.8
 netsh interface ipv4 add dns name="Ethernet0" 8.8.4.4 index=2
